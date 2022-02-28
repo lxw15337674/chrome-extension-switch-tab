87fcd6aa59b31e82033990cec510a886dd2de87e
weiji <weiji@qunhemail.com>
2022-02-28 18:31:05 +0800

    update

--- src/background.ts
+++ src/background.ts
@@ -4,12 +4,6 @@ const prefixRegEx = /^[0-9]\..*$/
 
 let keys:number[] = []
 
-const serializer = (value:any) => {
-  return JSON.stringify(value)
-}
-const deserializer = (value:any) => {
-  return JSON.parse(value);
-}
 
 async function toggleTab(key:string) {
   const tabId = keys[Number(key)]
@@ -25,28 +19,34 @@ async function getActiveTab() {
 }
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) =>{
-  console.log(request)
   if (request.action === 'toggleTab') {
     toggleTab(request.key)
   }
   if (request.action === 'setTab') {
     const tab = await getActiveTab()
-    keys[request.key] = tab.id as number
+    keysSet(request.key,tab.id as number)
   }
-  console.log(keys)
 });
 
 
 
 // keys定义为数组
-function keysSet(index:number, value:number) {
-  keys[index] = value
-  chrome.storage.local.set({ keys });
+function keysSet(index:number, tabId:number) {
+  // 如果已存在，则重置被覆盖tab的title
+  if(keys[index]){
+    sendClearNumber(keys[index])
+  }
+  keys[index] = tabId
+  sendSetNumber(tabId,index)
+  // chrome.storage.local.set({ keys });
 }
 
+const sendSetNumber= (tabId:number,key:number)=>{
+  tabs.sendMessage(tabId, { action: 'setNumber',key})
+}
 
-function getKeys():number[] {
-  return deserializer(chrome.storage.local.get(['keys']))
+const sendClearNumber=(tabId:number)=>{
+  tabs.sendMessage(tabId, { action: 'clearNumber'})
 }
 
 // TODO: 更新后重置storage
@@ -54,19 +54,28 @@ function getKeys():number[] {
 //   chrome.storage.local.set({ keys: [] })
 // })
 
+// 保持tab始终标记
+// tabs.onUpdated.addListener(function (tabId) {
+//   const key = keys.findIndex(item=>item===tabId)
+//   if(key!==-1){
+//    tabs.sendMessage(tabId, { action: 'setNumber',key})
+//   }
+// })
 
-tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  tabs.sendMessage(tabId, { action: 'set' }, (response) => {
-    console.log(
-      `background -> content script infos have been received. number: ${response.number}`
-    );
-  });
-})
+// chrome.windows.onCreated.addListener(() => {
+//   chrome.tabs.query({}, (tab)=>{
+//     console.log(tab)
+//   })
+  
+//   chrome.storage.local.get(['keys'],(v)=>{
+//     keys= v.keys
+//     keys.forEach((tabId,index)=>{
+//       if(tabId){
+//       tabs.sendMessage(tabId, { action: 'setNumber',index})
+//       }
+//     })
+//   })
+  
+// })
 
-chrome.windows.onCreated.addListener(() => {
-  let storageKeys = getKeys()
-  console.log(storageKeys)
-  if (storageKeys) {
-    keys = storageKeys
-  }
-})
+
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,35 +1,43 @@
 let isNum = (v:string) => /^\d+$/.test(v);
 const runtime = chrome.runtime
 
-
 const prefixReg = /^[0-9]\..*$/
 
 const setNumber = (number:number) => {
 	if (prefixReg.test(document.title)) {
+		if(Number(document.title[0])===number){
+			return 
+		}
 		document.title = `${number}${document.title.slice(1)}`
 	} else {
 		document.title = `${number}.${document.title}`
 	}
 }
 
+const clearNumber = ()=>{
+	if (prefixReg.test(document.title)) {
+		document.title=document.title.slice(2)
+	}
+}
 
 window.addEventListener('keydown', (e) => {
 	const { altKey, ctrlKey, key } = e
 	if (altKey && isNum(key)) {
 		e.preventDefault()
 		runtime.sendMessage({ action: 'toggleTab', key })
-	} if (ctrlKey && isNum(key)) {
-		runtime.sendMessage({ action: 'setTab', key }, () => {
-			setNumber(Number(key))
-		})
+	} 
+	if (ctrlKey && isNum(key)) {
+		runtime.sendMessage({ action: 'setTab', key })
 		e.preventDefault()
 	}
 })
 
-
-
-
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	console.log(request, sender, sendResponse)
+	if(request.action==='setNumber'){
+		setNumber(request.key)
+	}
+	if(request.action ==='clearNumber'){
+		clearNumber()
+	}
 })
