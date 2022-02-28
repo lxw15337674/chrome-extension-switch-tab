8d51abb1c6ade8fadebfd48ef105335b907dad6d
weiji <weiji@qunhemail.com>
2022-02-28 16:54:52 +0800

    update

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,72 @@
-function polling() {
-  console.log("polling");
-  // setTimeout(polling, 1000 * 300);
+const tabs = chrome.tabs
+const prefixRegEx = /^[0-9]\..*$/
+
+
+let keys:number[] = []
+
+const serializer = (value:any) => {
+  return JSON.stringify(value)
+}
+const deserializer = (value:any) => {
+  return JSON.parse(value);
 }
 
-polling(); 
-  
+async function toggleTab(key:string) {
+  const tabId = keys[Number(key)]
+  if (!tabId) { return }
+  await chrome.tabs.update(tabId, {
+    active: true,
+  })
+}
+
+async function getActiveTab() {
+  const [tab] =await tabs.query(({ active: true, currentWindow: true }))
+  return tab
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) =>{
+  console.log(request)
+  if (request.action === 'toggleTab') {
+    toggleTab(request.key)
+  }
+  if (request.action === 'setTab') {
+    const tab = await getActiveTab()
+    keys[request.key] = tab.id as number
+  }
+  console.log(keys)
+});
+
+
+
+// keys定义为数组
+function keysSet(index:number, value:number) {
+  keys[index] = value
+  chrome.storage.local.set({ keys });
+}
+
+
+function getKeys():number[] {
+  return deserializer(chrome.storage.local.get(['keys']))
+}
+
+// TODO: 更新后重置storage
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.local.set({ keys: [] })
+// })
+
+
+tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  tabs.sendMessage(tabId, { action: 'set' }, (response) => {
+    console.log(
+      `background -> content script infos have been received. number: ${response.number}`
+    );
+  });
+})
+
+chrome.windows.onCreated.addListener(() => {
+  let storageKeys = getKeys()
+  console.log(storageKeys)
+  if (storageKeys) {
+    keys = storageKeys
+  }
+})
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,35 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+let isNum = (v:string) => /^\d+$/.test(v);
+const runtime = chrome.runtime
+
+
+const prefixReg = /^[0-9]\..*$/
+
+const setNumber = (number:number) => {
+	if (prefixReg.test(document.title)) {
+		document.title = `${number}${document.title.slice(1)}`
+	} else {
+		document.title = `${number}.${document.title}`
+	}
+}
+
+
+window.addEventListener('keydown', (e) => {
+	const { altKey, ctrlKey, key } = e
+	if (altKey && isNum(key)) {
+		e.preventDefault()
+		runtime.sendMessage({ action: 'toggleTab', key })
+	} if (ctrlKey && isNum(key)) {
+		runtime.sendMessage({ action: 'setTab', key }, () => {
+			setNumber(Number(key))
+		})
+		e.preventDefault()
+	}
+})
+
+
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	console.log(request, sender, sendResponse)
+})
