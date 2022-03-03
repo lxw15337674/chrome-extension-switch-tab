541113ee957cfccecd6b830f7c2a9ce5f8fc41d6
weiji <weiji@qunhemail.com>
2022-03-03 15:14:29 +0800

    fix

--- src/background.ts
+++ src/background.ts
@@ -1,131 +1,117 @@
 const tabs = chrome.tabs
 const prefixRegEx = /^[0-9]\..*$/
 
-interface TabInfo{
-  windowId:number,
-  tabId:number
+interface TabInfo {
+  windowId: number,
+  tabId: number
 }
-let keys:(TabInfo|undefined)[] = []
+let keys: (TabInfo | undefined)[] = []
 
 
-async function toggleTab(key:string|number) {
+async function toggleTab(key: string | number) {
   const tabInfo = keys[Number(key)]
-  if(!tabInfo){
-    return 
+  if (!tabInfo) {
+    return
   }
-  await chrome.windows.update(tabInfo.windowId,{focused:true})
+  await chrome.windows.update(tabInfo.windowId, { focused: true })
   await chrome.tabs.update(tabInfo.tabId, {
     active: true,
   })
 }
 
+function findKey(windowId: number, tabId: number): number {
+  for (let i = 1; i < 10; i++) {
+    const tabInfo = keys[i]
+    if (tabInfo) {
+      if (tabInfo.windowId === windowId && tabInfo.tabId === tabId) {
+        return i
+      }
+    }
+  }
+  return -1
+}
+
 async function getActiveTab() {
-  const [tab] =await tabs.query(({ active: true, currentWindow: true }))
+  const [tab] = await tabs.query(({ active: true, currentWindow: true }))
   return tab
 }
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) =>{
-  
-  if (request.action === 'toggleTab') {
-    toggleTab(request.key)
-  }
-  if (request.action === 'setTab') {
-    setTab(request.key)
+chrome.runtime.onMessage.addListener(async (request, { tab }, sendResponse) => {
+  if (request.action === 'checkNumber') {
+    const key = findKey(tab?.windowId as number, tab?.id as number)
+    sendResponse(key)
   }
 });
 
-async function setTab(key:number){
+async function setTab(key: number) {
   const tab = await getActiveTab()
-  const window  =await  chrome.windows.getCurrent()
-  if(tab&&window){
-  keysSet(key,{tabId:tab.id as number,windowId:window.id as number})
+  const window = await chrome.windows.getCurrent()
+  if (tab && window) {
+    keysSet(key, { tabId: tab.id as number, windowId: window.id as number })
   }
 }
 
 
 // keys定义为数组
-function keysSet(index:number, tabInfo:TabInfo) {
+function keysSet(index: number, tabInfo: TabInfo) {
   // 如果已存在，则重置被覆盖tab的title
-  const tabId =keys[index]?.tabId
-  if(tabId){
+  const tabId = keys[index]?.tabId
+  if (tabId) {
     sendClearNumber(tabId)
   }
   // 如果重新编号，则删除之前的编号
-  let existedIndex = keys.findIndex(item=>item?.tabId===tabInfo.tabId&&item?.windowId===tabInfo.windowId)
-  if(existedIndex>=0){
+  let existedIndex = keys.findIndex(item => item?.tabId === tabInfo.tabId && item?.windowId === tabInfo.windowId)
+  if (existedIndex >= 0) {
     const tabId = keys[existedIndex]?.tabId as number
     sendClearNumber(tabId)
     keys[existedIndex] = undefined
   }
-  keys[index]=tabInfo
-  sendSetNumber(tabInfo.tabId,index)
+  keys[index] = tabInfo
+  sendSetNumber(tabInfo.tabId, index)
 }
 
-const sendSetNumber= (tabId:number,key:number)=>{
-  tabs.sendMessage(tabId, { action: 'setNumber',key})
+const sendSetNumber = (tabId: number, key: number) => {
+  tabs.sendMessage(tabId, { action: 'setNumber', key })
 }
 
-const sendClearNumber=(tabId:number)=>{
-  tabs.sendMessage(tabId, { action: 'clearNumber'})
+const sendClearNumber = (tabId: number) => {
+  tabs.sendMessage(tabId, { action: 'clearNumber' })
 }
 
-// TODO: 更新后重置storage
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.local.set({ keys: [] })
-// })
 
-// 保持tab始终标记
-// tabs.onUpdated.addListener(function (tabId) {
-//   const key = keys.findIndex(item=>item===tabId)
-//   if(key!==-1){
-//    tabs.sendMessage(tabId, { action: 'setNumber',key})
-//   }
-// })
 
-// chrome.windows.onCreated.addListener(() => {
-//   chrome.tabs.query({}, (tab)=>{
-//     
-//   })
-  
-//   chrome.storage.local.get(['keys'],(v)=>{
-//     keys= v.keys
-//     keys.forEach((tabId,index)=>{
-//       if(tabId){
-//       tabs.sendMessage(tabId, { action: 'setNumber',index})
-//       }
-//     })
-//   })
-  
-// })
-
-const reloadAllTab = async ()=>{
+const reloadAllTab = async () => {
   const allTabs = await tabs.query({})
-  for(let tab of allTabs){
-    if(tab.id){
-  tabs.reload(tab.id)
+  for (let tab of allTabs) {
+    if (tab.id) {
+      tabs.reload(tab.id)
     }
   }
 }
 
-chrome.runtime.onInstalled.addListener( ()=>{
-  reloadAllTab()
+chrome.commands.onCommand.addListener((command, tab) => {
+  if (/switch./.test(command)) {
+    toggleTab(command[command.length - 1])
+    return
+  }
+  if (
+    /set./.test(command)
+  ) {
+    const number = Number(command[command.length - 1])
+    chrome.notifications.clear('setTab');
+    chrome.notifications.create('setTab', {
+      type: 'basic',
+      iconUrl: chrome.runtime.getURL(`icon.png`),
+      title: '设置成功',
+      message: `设置当前标签页为${number}`,
+      priority: 0,
+      silent: true
+    }, (notificationId) => {
+      setTimeout(function () {
+        chrome.notifications.clear(notificationId);
+      }, 2000);
+    })
+    setTab(number)
+  }
 })
 
-
-chrome.commands.onCommand.addListener((command,tab) => {
-  switch (command){
-    case 'switch1':
-      toggleTab(1)
-      break;
-    case 'switch2':
-      toggleTab(2)
-      break
-    case 'set1':
-      setTab(1)
-      break
-    case 'set2':
-      setTab(2)
-      break
-  }
-  
-});
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,44 +1,56 @@
-let isNum = (v:string) => /^\d+$/.test(v);
-const runtime = chrome.runtime
+let isNum = (v: string) => /^\d+$/.test(v);
+const runtime = chrome.runtime;
 
-const prefixReg = /^[0-9]\..*$/
+const prefixReg = /^[0-9]\..*$/;
 
-const setNumber = (number:number) => {
-	if (prefixReg.test(document.title)) {
-		if(Number(document.title[0])===number){
-			return 
-		}
-		document.title = `${number}${document.title.slice(1)}`
-	} else {
-		document.title = `${number}.${document.title}`
-	}
-}
+const setNumber = (number: number) => {
+  if (prefixReg.test(document.title)) {
+    if (Number(document.title[0]) === number) {
+      return;
+    }
+    document.title = `${number}${document.title.slice(1)}`;
+  } else {
+    document.title = `${number}.${document.title}`;
+  }
+};
 
-const clearNumber = ()=>{
-	if (prefixReg.test(document.title)) {
-		document.title=document.title.slice(2)
-	}
-}
+const clearNumber = () => {
+  if (prefixReg.test(document.title)) {
+    document.title = document.title.slice(2);
+  }
+};
 
-window.addEventListener('keydown', (e) => {
-	const { altKey, ctrlKey, key } = e
-	if (ctrlKey && isNum(key)) {
-		runtime.sendMessage({ action: 'setTab', key })
-		e.preventDefault()
-		return 
-	}
-	if (altKey && isNum(key)) {
-		e.preventDefault()
-		runtime.sendMessage({ action: 'toggleTab', key })
-		return 
-	} 
-})
+runtime.onMessage.addListener((request) => {
+  if (request.action === 'setNumber') {
+    setNumber(request.key);
+  }
+  if (request.action === 'clearNumber') {
+    clearNumber();
+  }
+});
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if(request.action==='setNumber'){
-		setNumber(request.key)
-	}
-	if(request.action ==='clearNumber'){
-		clearNumber()
-	}
-})
+const checkNumber = () => {
+  runtime.sendMessage({ action: 'checkNumber' }, (key) => {
+    if (key > 0) {
+      setNumber(key);
+    }
+  });
+};
+
+const keepMarked = () => {
+  checkNumber();
+  // 保持tab的标记
+  const target = document.querySelector('title') as HTMLTitleElement;
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function () {
+      checkNumber();
+    });
+  });
+
+  const config = {
+    childList: true,
+  };
+  observer.observe(target, config);
+};
+
+keepMarked();
